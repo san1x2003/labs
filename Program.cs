0c16e812ce4e7c3dad8704d6ee38c9160bf79620
san1x2003 <105587997+san1x2003@users.noreply.github.com>
2022-11-10 22:09:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,56 +1,139 @@
-﻿using System;
-
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace Calcul
+namespace CALCULATOR
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double b;
-            double a;
-            Console.WriteLine("Введите знак");
-            Console.WriteLine("Введи 1 чтобы прибавить");
-            Console.WriteLine("Введи 2 чтобы вычесть");
-            Console.WriteLine("Введи 3 чтобы умножить");
-            Console.WriteLine("Введи 4 чтобы разделить \n");
-            int action = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введи первое число");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введи второе число");
-            b = Convert.ToDouble(Console.ReadLine());
-            double result = 0;
-            switch (action)
+            while (true) 
             {
-                case 1:
-                    {
-                        result = a + b;
-                        break;
-                    }
-                case 2:
-                    {
-                        result = a - b;
-                        break;
-                    }
-                case 3:
-                    {
-                        result = a * b;
-                        break;
-                    }
-                case 4:
-                    {
-                        result = a / b;
-                        break;
-                    }
-                default:
-                    Console.WriteLine("Не верно");
-                    break;
+                Console.Write("Введите выражение: "); 
+                Console.WriteLine(ReversePolishNotation.Calculate(Console.ReadLine())); 
             }
-            Console.WriteLine("Результат {0}", result);
-            Console.ReadKey();
         }
+    }
+    class ReversePolishNotation
+    {
+        static private bool IsDelimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {          
+                case '+': return 1;
+                case '-': return 2;
+                case '*': return 3;
+                case '/': return 3;
+                default: return 4;
+            }
+        }
+        static public double Calculate(string input)
+        {
+            string output = ConvertExpression(input); 
+            double result = Counting(output); 
+            return result; 
+        }
+        static private string ConvertExpression(string input)
+        {
+            string output = string.Empty; 
+            Stack<char> operStack = new Stack<char>(); 
 
+            for (int i = 0; i < input.Length; i++)
+            {
+             
+                if (IsDelimeter(input[i]))
+                    continue;
 
+                
+                if (Char.IsDigit(input[i])) 
+                {
+                    
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i]; 
+                        i++; 
+
+                        if (i == input.Length) break; 
+                    }
+
+                    output += " ";
+                    i--; 
+                }
+
+                
+                if (IsOperator(input[i])) 
+                {
+                    if (operStack.Count > 0) 
+                        if (GetPriority(input[i]) <= GetPriority(operStack.Peek())) 
+                            output += operStack.Pop().ToString() + " "; 
+                    operStack.Push(char.Parse(input[i].ToString())); 
+
+                }
+            }
+
+            
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output; 
+        }
+        static private double Counting(string input)
+        {
+            double result = 0;
+            Stack<double> temp = new Stack<double>(); 
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+                
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i])) 
+                    {
+                        a += input[i]; 
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a)); 
+                    i--;
+                }
+                else if (IsOperator(input[i])) 
+                {
+                  
+                    double a = temp.Pop();
+                    double b = temp.Pop();
+
+                    switch (input[i]) 
+                    {
+                        case
+                    '+':
+                            result = b + a; break;
+                        case '-': result = b - a; break;
+                        case '*': result = b * a; break;
+                        case '/':
+                            result = b /
+                            a; break;
+
+                    }
+                    temp.Push(result); 
+                }
+            }
+            return temp.Peek(); 
+        }
     }
 }
